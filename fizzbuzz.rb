241184b82221b39f373ca4cab082ba07ab7e6305
victoriaehrbar <victoriaehrbar@gmail.com>
2020-10-26 23:33:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,4 @@
 def fizzbuzz
 
-end
-
-// ♥ irb
-2.2.3 :001 > 1+1
- => 2
-  Don't forget! This file needs to be 'required' in its spec file
+end# Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
