9055854da17e6d553aa5817ce43092a2c7fafa72
victoriaehrbar <victoriaehrbar@gmail.com>
2020-10-26 23:32:58 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,9 @@
-# Don't forget! This file needs to be 'required' in its spec file
+def fizzbuzz
+
+end
+
+// ♥ irb
+2.2.3 :001 > 1+1
+ => 2
+  Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
